82b77d8a25144f8f5e3cc26c1c8685764aaae589
Oscar Ocampo <oscarocampo76@hotmail.com>
2017-08-17 04:28:52 +0000

    First commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Medoo;
+require_once '../vendor/autoload.php';
+
+echo "Hello World";
+
+$database = new medoo([
+    'database_type' => 'sqlite',
+    'database_file' => '../storage/database.db'
+]);
+
+?>
